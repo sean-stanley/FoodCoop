d4d6c966e42cd7d01dd2fcc4c4a6deeedb9dad3d
Sean Stanley <sean@foodcoop.org.nz>
2013-08-15 13:46:44 +1200

    first draft of forms and new popup menu using angularjs

--- angular/angular-seed-master/app/js/app.js
+++ angular/angular-seed-master/app/js/app.js
@@ -6,5 +6,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/signup', {templateUrl: 'partials/signup.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/product-upload', {templateUrl: 'partials/product-upload.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
--- angular/angular-seed-master/app/js/controllers.js
+++ angular/angular-seed-master/app/js/controllers.js
@@ -8,4 +8,7 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('Login', ['$scope', function($scope) {
+	  $scope.showLogin = false;
   }]);
