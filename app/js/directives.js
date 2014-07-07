8299bb4c87bf80238c0cbcdc0ccc3bda554ae8f3
Sean Stanley <sean@foodcoop.org.nz>
2014-07-08 10:22:24 +1200

    cleaned up directives

--- app/js/directives.js
+++ app/js/directives.js
@@ -106,45 +106,6 @@ angular.module('co-op.directives', []).
       };
     })
   
-  .directive('fundooRating', function () { //www.\befundoo.com/university/tutorials/angularjs-directives-tutorial
-    return {
-      restrict: 'A',
-      template: '<ul class="rating">' +
-                  '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
-                    '\u2605' +
-                  '</li>' +
-                '</ul>',
-      scope: {
-        ratingValue: '=',
-        max: '=',
-        readonly: '@',
-        onRatingSelected: '&'
-      },
-      link: function (scope, elem, attrs) {
-
-        var updateStars = function() {
-          scope.stars = [];
-          for (var  i = 0; i < scope.max; i++) {
-            scope.stars.push({filled: i < scope.ratingValue});
-          }
-        };
-
-        scope.toggle = function(index) {
-          if (scope.readonly && scope.readonly === 'true') {
-            return;
-          }
-          scope.ratingValue = index + 1;
-          scope.onRatingSelected({rating: index + 1});
-        };
-
-        scope.$watch('ratingValue', function(oldVal, newVal) {
-          if (newVal) {
-            updateStars();
-          }
-        });
-       }
-      };
-     })
   
   .directive('checkStrength', function () {
 
@@ -217,66 +178,67 @@ angular.module('co-op.directives', []).
   
   
   .directive('fileDropzone', function() {
-    return {
-      restrict: 'A',
-      scope: {
-        file: '=',
-        fileName: '='
-      },
-      link: function(scope, element, attrs) {
-        var checkSize, isTypeValid, processDragOverOrEnter, validMimeTypes;
-        processDragOverOrEnter = function(event) {
-          if (event !== null) {
-            event.preventDefault();
-          }
-          event.originalEvent.dataTransfer.effectAllowed = 'copy';
-          return false;
-        };
-        validMimeTypes = attrs.fileDropzone;
-        checkSize = function(size) {
-          var _ref;
-          if (((_ref = attrs.maxFileSize) === (void 0) || _ref === '') || (size / 1024) / 1024 < attrs.maxFileSize) {
-            return true;
-          } else {
-            alert("File must be smaller than " + attrs.maxFileSize + " MB");
-            return false;
-          }
-        };
-        isTypeValid = function(type) {
-          if ((validMimeTypes === (void 0) || validMimeTypes === '') || validMimeTypes.indexOf(type) > -1) {
-            return true;
-          } else {
-            alert("Invalid file type.  File must be one of following types " + validMimeTypes);
-            return false;
-          }
-        };
-        element.bind('dragover', processDragOverOrEnter);
-        element.bind('dragenter', processDragOverOrEnter);
-        return element.bind('drop', function(event) {
-          var file, name, reader, size, type;
-          if (event !== null) {
-            event.preventDefault();
-          }
-          reader = new FileReader();
-          reader.onload = function(evt) {
-            if (checkSize(size) && isTypeValid(type)) {
-              return scope.$apply(function() {
-                scope.file = evt.target.result;
-                if (angular.isString(scope.fileName)) {
-                  scope.fileName = name;
-                  return name;
-                }
-              });
-            }
-          };
-          file = event.originalEvent.dataTransfer.files[0];
-          name = file.name;
-          type = file.type;
-          size = file.size;
-          reader.readAsDataURL(file);
-          return false;
-        });
-      }
-    };
-  });
+	return {
+	restrict: 'A',
+	scope: {
+		file: '=',
+		fileName: '='
+	},
+	link: function(scope, element, attrs) {
+		var checkSize, isTypeValid, processDragOverOrEnter, validMimeTypes;
+		processDragOverOrEnter = function(event) {
+			if (event !== null) {
+				event.preventDefault();
+			}
+			event.originalEvent.dataTransfer.effectAllowed = 'copy';
+			return false;
+		};
+		validMimeTypes = attrs.fileDropzone;
+		checkSize = function(size) {
+			var _ref;
+			if (((_ref = attrs.maxFileSize) === (void 0) || _ref === '') || (size / 1024) / 1024 < attrs.maxFileSize) {
+				return true;
+			} else {
+				alert("File must be smaller than " + attrs.maxFileSize + " MB");
+				return false;
+			}
+		};
+		isTypeValid = function(type) {
+			if ((validMimeTypes === (void 0) || validMimeTypes === '') || validMimeTypes.indexOf(type) > -1) {
+				return true;
+			} else {
+				alert("Invalid file type.  File must be one of following types " + validMimeTypes);
+				return false;
+			}
+		};
+		element.bind('dragover', processDragOverOrEnter);
+		element.bind('dragenter', processDragOverOrEnter);
+		return element.bind('drop', function(event) {
+			var file, name, reader, size, type;
+			if (event !== null) {
+				event.preventDefault();
+			}
+			reader = new FileReader();
+			reader.onload = function(evt) {
+				if (checkSize(size) && isTypeValid(type)) {
+					return scope.$apply(function() {
+						scope.file = evt.target.result;
+						if (angular.isString(scope.fileName)) {
+							scope.fileName = name;
+							return name;
+						}
+					});
+				}
+			};
+			file = event.originalEvent.dataTransfer.files[0];
+			name = file.name;
+			type = file.type;
+			size = file.size;
+			reader.readAsDataURL(file);
+			return false;
+		});
+	}
+};
+});
+
   
