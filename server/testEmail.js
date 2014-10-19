609c765e33fa6245400baf58e91cab10ca368863
Sean Stanley <sean@foodcoop.org.nz>
2014-10-20 11:06:00 +1300

    added policy statements pdf
    also changed welcome page and admin design.

---
+++ server/testEmail.js
@@ -0,0 +1,48 @@
+var Emailer = require('./emailer'),
+	adminRecipients = [' "Sean Stanley" <sean@maplekiwi.com>', '"Michael Taylor" <michael@maplekiwi.com>'],
+	companyEmail = {email: 'sean@maplekiwi.com', name: 'Sean Stanley'};
+
+function currentTime() {
+	  var date, hour, min;
+	  date = new Date();
+	  hour = date.getHours();
+	  min = date.getMinutes();
+	  if (min < 10) {
+		 min = '0' + min.toString();
+	  }
+	  return hour + ":" + min;
+  }
+  
+var currentTimeString = currentTime();
+
+var options = {
+	template: "server-start",
+	subject: "Server up and running",
+	to: companyEmail
+};
+
+var data = {
+  name: "Sean Stanley",
+  AngularVersion: "1.2.26",
+  expressVersion: "4.9.1",
+  currentTime: currentTimeString,
+  data: [1, 2, 3, 4, 5]
+  };
+
+serverStart = new Emailer(options, data);
+
+
+
+
+serverStart.send(function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Message sent")
+});
+
+
+
+exports.testEmail = serverStart;
+exports.adminEmail = adminRecipients;
+exports.companyEmail = companyEmail;
